44015efbdea639ef5bec6af90103639a0fd6882c
Brian Mock <mock.brian@gmail.com>
2012-11-20 22:36:47 -0800

    Now using underscore.js and jQuery

--- src/main.js
+++ src/main.js
@@ -1,25 +1,12 @@
-window.onload = function() {
-var elems = {
-    table: document.getElementById('theTable'),
-    tbody: document.getElementById('theTableBody')
-};
-
+(function() {
+var util = {};
 var table = [];
 var size = 10;
 
 var cursor = {row: 0, col: 0};
 
-var slice = function(ary, n) {
-    return Array.prototype.slice.call(ary, n || 0);
-};
-
-var times = function(n, f) {
-    var i;
-    for (i = 0; i < n; i++) f(i);
-};
-
-var format = function(str /*, ... */) {
-    var args = slice(arguments, 1);
+util.format = function(str /*, ... */) {
+    var args = _.toArray(arguments).slice(1);
     var i = 0;
     var n = args.length;
     return str.replace(/{\d+}/g, function(tok) {
@@ -28,38 +15,10 @@ var format = function(str /*, ... */) {
     });
 };
 
-times(size, function(x) {
-    var tr = document.createElement('tr');
-    var row = [];
-    times(size, function(y) {
-        var td = document.createElement('td');
-        // var text = document.createTextNode(format('{0}{1}', x, y));
-        td.setAttribute('data-row', y);
-        td.setAttribute('data-col', x);
-        // td.appendChild(text);
-        tr.appendChild(td);
-        row.push(td);
-    });
-    elems.tbody.appendChild(tr);
-    table.push(row);
-});
-
-elems.table.onmousemove = function(event) {
-    var target = event.target;
-
-    if (target && target.nodeName !== 'TD')
-        return;
-
-    var row = target.getAttribute('data-row') >>> 0;
-    var col = target.getAttribute('data-col') >>> 0;
-
-    moveCursor(row, col);
-};
-
 var highlight = function(row, col) {
     var classes = {2: 'selected', 1: 'crosshair'};
-    times(size, function(r) {
-        times(size, function(c) {
+    _.times(size, function(r) {
+        _.times(size, function(c) {
             var td = table[c][r];
             var R = row === r;
             var C = col === c;
@@ -68,7 +27,7 @@ var highlight = function(row, col) {
     });
 };
 
-var clamp = function(x, a, b) {
+util.clamp = function(x, a, b) {
     var max = Math.max;
     var min = Math.min;
 
@@ -76,8 +35,8 @@ var clamp = function(x, a, b) {
 };
 
 var moveCursor = function(row, col) {
-    row = clamp(row | 0, 0, size - 1);
-    col = clamp(col | 0, 0, size - 1);
+    row = util.clamp(row | 0, 0, size - 1);
+    col = util.clamp(col | 0, 0, size - 1);
 
     cursor.row = row;
     cursor.col = col;
@@ -85,19 +44,47 @@ var moveCursor = function(row, col) {
     highlight(row, col);
 };
 
-moveCursor(1, 1);
-
 var translateCursor = function(drow, dcol) {
     moveCursor(cursor.row + drow, cursor.col + dcol);
 };
 
-document.onkeydown = function(event) {
-    var key = event.keyCode;
-    switch (key) {
-    case 73: translateCursor( 0, -1); break;
-    case 74: translateCursor(-1,  0); break;
-    case 75: translateCursor( 0, +1); break;
-    case 76: translateCursor(+1,  0); break;
-    };
+var loadGame = function() {
+    _.times(size, function(x) {
+        var tbody = $('#theTableBody');
+        var tr = $('<tr>');
+        var row = [];
+        _.times(size, function(y) {
+            var td = $('<td>');
+            td.data('row', y);
+            td.data('col', x);
+            tr.append(td);
+            row.push(td[0]);
+        });
+        tbody.append(tr);
+        table.push(row);
+    });
+
+    moveCursor(1, 1);
+
+    $('#theTable').on('mousemove', 'td', function(event) {
+        var target = $(event.target);
+
+        var row = target.data('row') >>> 0;
+        var col = target.data('col') >>> 0;
+
+        moveCursor(row, col);
+    });
+
+    $(document).keydown(function(event) {
+        var key = event.keyCode;
+        switch (key) {
+        case 73: translateCursor( 0, -1); break;
+        case 74: translateCursor(-1,  0); break;
+        case 75: translateCursor( 0, +1); break;
+        case 76: translateCursor(+1,  0); break;
+        };
+    });
 };
-};
+
+$(loadGame);
+})();
