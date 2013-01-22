60874935d6f6760fc8e94936fb8cb297f99cd6bc
Brian Mock <mock.brian@gmail.com>
2013-01-22 00:09:13 -0800

    Massive CSS cleanup

--- src/colors.js
+++ src/colors.js
@@ -11,39 +11,47 @@ var yellow  =  40;
 var cyan    = 180;
 var blue    = 200;
 var purple  = 300;
-var green   =  80;
+var green   =  70;
 var aqua    = 100;
 
+// var hue1 = 200;
+// var hue2 = (hue1 + 180 + 10) % 360;
+// var hue3 = (hue1 + 180 - 10) % 360;
+
+var hue1 = blue;
+var hue2 = yellow;
+var hue3 = green;
+
 return {
-    highlight:      hsla(cyan, 0.3, 0.50, 1.00),
-    cursorShadow:   hsla(cyan, 0.4, 0.40, 1.00),
-    crosshair:      hsla(cyan, 0.4, 0.40, 0.20),
-    crosshairShadow:hsla(cyan, 0.4, 0.40, 0.30),
+    highlight:      hsla(hue1, 0.3, 0.50, 1.00),
+    cursorShadow:   hsla(hue1, 0.4, 0.40, 1.00),
+    crosshair:      hsla(hue1, 0.4, 0.40, 0.20),
+    crosshairShadow:hsla(hue1, 0.4, 0.40, 0.30),
     // cursorShadow:   alphaGray(0, 0.20),
     shadeCell:      alphaGray(0, 0.10),
     shadeLess:      alphaGray(0, 0.05),
     shadeLine:      alphaGray(0, 0.20),
-    filled:         hsl(green, 0.4, 0.6),
-    background:     hsl(yellow, 0.9, 0.97),
-    marked:         hsl(yellow, 0.6, 0.60),
-    minorLines:     hsl(yellow, 0.8, 0.90),
-    majorLines:     hsl(yellow, 0.7, 0.70),
-    outsideBorder:  hsl(yellow, 0.5, 0.65),
+    filled:         hsl(hue3, 0.4, 0.6),
+    background:     hsl(hue2, 0.9, 0.97),
+    marked:         hsl(hue2, 0.6, 0.60),
+    minorLines:     hsl(hue2, 0.8, 0.90),
+    majorLines:     hsl(hue2, 0.7, 0.70),
+    outsideBorder:  hsl(hue2, 0.5, 0.65),
     // insetShadow:    alphaGray(0, 0.05),
     insetShadow:    alphaGray(0, 0.40),
-    outerShadow:    hsla(aqua, 0.6, 0.3, 0.20),
-    minimapBG:      hsl (aqua, 0.6, 0.9),
+    outerShadow:    hsla(hue1, 0.6, 0.3, 0.20),
+    minimapBG:      hsl (hue1, 0.6, 0.9),
     innerShadow:    alphaGray(0, 0.20),
-    hintsBG:        hsla(blue, 0.3, 0.7, 0.25),
-    hintsFade:      hsla(blue, 0.3, 0.7, 0.00),
-    hintsSelBG:     hsla(cyan, 0.4, 0.40, 0.10),
-    hintsSelFade:   hsla(cyan, 0.4, 0.40, 0.00),
+    hintsBG:        hsla(hue1, 0.3, 0.7, 0.25),
+    hintsFade:      hsla(hue1, 0.3, 0.7, 0.00),
+    hintsSelBG:     hsla(hue1, 0.4, 0.40, 0.10),
+    hintsSelFade:   hsla(hue1, 0.4, 0.40, 0.00),
     shadow:         alphaGray(0,   0.25),
     light:          alphaGray(255, 0.75),
     shadowX:        alphaGray(0,   0.00),
-    cellShade:      rgba(100, 110, 120, 0.05),
-    fontNormal:     hsl(blue,   0.1, 0.4),
-    fontSelected:   hsl(cyan, 0.3, 0.3),
+    cellShade:      alphaGray(0,   0.03),
+    fontNormal:     hsl(hue1,   0.1, 0.4),
+    fontSelected:   hsl(hue1, 0.3, 0.3),
     fontStroke:     alphaGray(0, 0.25),
     // fontNormal:     hsl(yellow, 0.5, 0.4),
     // fontNormal:     alphaGray(0, 0.7),
